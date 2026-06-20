f80ed7da78399bf9968d0c2805a5bc544697a576
Szymon Kownacki <szymon.kownacki@gmail.com>
2026-06-20 17:08:09 +0200

    chore: run published GHCR image in tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-podman run --rm -v "$PWD/data:/app/data:z" portfolio-tracker "$@"
+VERSION=$(grep '^version' "$(dirname "$0")/pyproject.toml" | sed 's/version = "\(.*\)"/\1/')
+podman run --rm -v "$PWD/data:/app/data:z" ghcr.io/simonko9/sbeacon:"$VERSION" "$@"
